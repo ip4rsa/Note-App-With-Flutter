39ce7fe1d70741e457d6d39694fe892f8a18ec68
ip4rsa <ip4rsa@gmail.com>
2023-10-22 14:14:15 +0330

    Update

--- lib/home_Screen.dart
+++ lib/home_Screen.dart
@@ -26,16 +26,17 @@ class _homeScreenState extends State<homeScreen> {
                 controller: _controller,
               ),
               Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(_text),
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  box.get(2) ?? 'Null',
+                ),
               ),
               ElevatedButton(
                 onPressed: () {
-                  setState(() {
-                    _text = _controller.text;
-                  });
+                  box.put(2, _controller.text);
+                  setState(() {});
                 },
-                child: Text('show text'),
+                child: const Text('show text'),
                 onLongPress: () {
                   setState(
                     () {
@@ -45,16 +46,28 @@ class _homeScreenState extends State<homeScreen> {
                 },
               ),
               ElevatedButton(
-                child: Text('Create'),
+                child: const Text('Create'),
                 onPressed: () {
                   box.put(1, 'Parsa');
                 },
               ),
               ElevatedButton(
-                child: Text('Read'),
+                child: const Text('Read'),
                 onPressed: () {
                   var getText = box.get(1);
                   print(getText);
+                },
+              ),
+              ElevatedButton(
+                child: const Text('update'),
+                onPressed: () {
+                  box.put(1, 'Sajjadian');
+                },
+              ),
+              ElevatedButton(
+                child: const Text('delete'),
+                onPressed: () {
+                  box.delete(1);
                 },
               ),
             ],
