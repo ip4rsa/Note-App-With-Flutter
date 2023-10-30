6c8be3a87864920f8bb38b597408e8533de704b7
ip4rsa <ip4rsa@gmail.com>
2023-10-30 12:39:12 +0330

    Update

--- lib/task_widget.dart
+++ lib/task_widget.dart
@@ -13,7 +13,6 @@ class _TaskWidgetState extends State<TaskWidget> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     isBoxCheckd = widget.task.isDone;
   }
