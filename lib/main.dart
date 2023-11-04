87cfc4aa3b64a212e0bcae5c30985babdf732611
ip4rsa <ip4rsa@gmail.com>
2023-11-04 16:51:11 +0330

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:note_app/constants/colors.dart';
 import 'package:note_app/data/task.dart';
 import 'package:note_app/data/taskType_Enum.dart';
 import 'package:note_app/data/task_taype_model.dart';
-import 'package:note_app/Screen/home_Screen.dart';
 
 void main() async {
   await Hive.initFlutter();
